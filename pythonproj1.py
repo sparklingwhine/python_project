1133726df669d64eeea2a6832f8c7f5cce23e280
Michelle Aubrey <michelle.e.aubrey@gmail.com>
2021-02-01 19:36:12 -0500

    new repo commit

---
+++ pythonproj1.py
@@ -0,0 +1,24 @@
+# Basic (Cat? Manatee?) Character Creator
+
+char_attribs = []
+print("Welcome to Michelle's Character Generator! \n Have fun!")
+
+# Choose gender (options: male, female, nonbinary)
+
+gender = input("What gender is your character?")
+
+# Choose size (options: large, medium, small)
+
+size = input("What size is your character?")
+
+# Choose color(s) pattern (options: single color, multi-color)
+
+color_pattern = input("What color-pattern does your character have?")
+
+# Choose age (1-100)
+
+age = input("How old is your character?") 
+
+# Choose name (freeform)
+
+name = input("What is your character's name?")
